ad83782e0edb9844a9530808d407d954cd87b5cd
Glen Wasserfall <glen@wasserfalls.co.za>
2019-10-30 21:38:59 +0200

    added login controller

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,16 @@
+<?php 
+
+class SignupController extends BaseController
+{
+
+	public function __construct($name, $args)
+	{
+		parent::__construct($name, $args);
+		$this->model = new UserModel();
+	}
+
+	public function default()
+	{
+		RenderView::file("Login");
+	}
+}
