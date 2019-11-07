33e1c8ac243d60bcca3d88323d8550dd7fb433b5
Glen Wasserfall <glen@wasserfalls.co.za>
2019-11-07 11:34:38 +0200

    adding webcam and file upload

--- app/Views/Snippets/AddPost.php
+++ app/Views/Snippets/AddPost.php
@@ -4,24 +4,78 @@
     </header>
     <section class="modal-card-body">
 
-	
+		<div class="columns is-centered">
+			<div class="column">
+				<div id="step-start" class="step active">
+					<div class="post-selection">
+						<div><a onclick="stepFile()" class="button is-primary">Image</a></div>
+						<div class="post-selection-divider">
+							<div></div>
+							<div>OR</div>
+							<div></div>
+							<sample-div>Sexy</sample-div>
+						</div>
+						<div><a onclick="stepWebcam()" class="button is-primary">Webcam</a></div>
+					</div>
+				</div>
+
+				<div id="step-file" class="step">
+					<div class="post-selection">
+						<div class="canvas-display">
+							<canvas id="file-preview"></canvas>
+						</div>
+						<div class="file is-boxed">
+							<label class="file-label">
+								<input id="file-input" class="file-input" type="file" accepts="image/*" name="image">
+								<span class="file-cta">
+									<span class="file-icon">
+										<i class="fas fa-upload"></i>
+									</span>
+									<span class="file-label">
+										Choose a file…
+									</span>
+								</span>
+							</label>
+						</div>
+					</div>
+				</div>
+
+				<div id="step-webcam" class="step">
+					<div class="post-selection">
+						<canvas id="webcam-preview"></canvas>
+					</div>
+				</div>
+
+				<div id="step-canvas" class="step">
+					<div class="post-selection">
+						<div><a class="button is-primary">Image</a></div>
+						<div class="post-selection-divider">
+							<div></div>
+							<div>OR</div>
+							<div></div>
+						</div>
+						<div><a class="button is-primary">Webcam</a></div>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+
+
 	<div class="step">
-		select image or upload here
-		<input type="file" class="file">
-	</div>
-
-	<div id="post-file-upload">
-
-	</div>
-
-	<div id="post-video">
-
-		<video autoplay></video>
+		<div id="post-file-upload">
+	
+		</div>
+	
+		<div id="post-video">
+			<video autoplay></video>
+		</div>
 	</div>
 
 
 
-
+	<link rel="stylesheet" href="/css/snippets/add-post.css">
 	<script src="/js/snippets/add-post.js"></script>
     </section>
     <!-- <footer class="modal-card-foot is-vcentered">
--- public/js/snippets/add-post.js
+++ public/js/snippets/add-post.js
@@ -8,4 +8,66 @@ console.log("hello");
 
 // navigator.mediaDevices.getUserMedia(constraints)
 // 	.then((stream) => {video.srcObject = stream});
-  
+
+
+step_start = document.getElementById("step-start")
+step_file  = document.getElementById("step-file")
+step_webcam  = document.getElementById("step-webcam")
+step_canvas  = document.getElementById("step-canvas")
+
+file_preview = document.getElementById("file-preview")
+file_context = file_preview.getContext("2d")
+file_input = document.getElementById("file-input")
+
+webcam_preview = document.getElementById("webcam-preview")
+webcam_context = file_preview.getContext("2d")
+
+
+
+steps = [step_start, step_file, step_webcam, step_canvas]
+
+current_step = step_start
+
+function stepFile()
+{
+	current_step.classList.remove("active")
+	current_step = step_file
+	current_step.classList.add("active")
+}
+
+function stepWebcam()
+{
+	current_step.classList.remove("active")
+	current_step = step_webcam
+	current_step.classList.add("active")
+}
+
+file_input.addEventListener("change", (e) => {
+	if ( file_input.files && file_input.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+           var img = new Image();
+           img.addEventListener("load", function() {
+				clearCanvas(file_context, file_preview)
+            	file_context.drawImage(img, 70, 20, 600, 600, 0, 0, 600, 600);
+           });
+           img.src = e.target.result;
+        };       
+        FR.readAsDataURL( file_input.files[0] );
+    }
+}, false);
+
+function clearCanvas(context, canvas) {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	var w = canvas.width;
+	canvas.width = 1;
+	canvas.width = w;
+}
+
+
+
+
+// class CanvasImage
+// {
+
+// }
