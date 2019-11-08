67004f3eb6af5d9243185d7fa1c4192883989a6c
Rachel BOLTON <rbolton@student.wethinkcode.co.za>
2019-11-08 17:17:45 +0200

    promise, leaving the footers alone after this...

--- app/Views/Components/Desktop/SignedInFooter-desktop.php
+++ app/Views/Components/Desktop/SignedInFooter-desktop.php
@@ -33,47 +33,50 @@
   </nav>
 <div>
 
-<nav class="level navbar is-light is-fixed-bottom is-hidden-touch" role="navigation" aria-label="main navigation">
-  <div class="level-left navbar-brand">
-    <a class="navbar-item" href="/home">
-      <img src="../../img/Camagru-transp-long5.png" alt="Home">
-    </a>
-  </div>
-  <div id="navbarMenu" class="level-right navbar-menu">
-    <div class="level-item navbar-start">
-      <a onclick="loadSnippet('ContactUs')" class="level-item navbar-item">
-        Contact Us
+<div class="is-hidden-touch">
+  <div style="margin-top: 3.5rem;"></div>
+  <nav class="level navbar is-light is-fixed-bottom is-hidden-touch" role="navigation" aria-label="main navigation">
+    <div class="level-left navbar-brand">
+      <a class="navbar-item" href="/home">
+        <img src="../../img/Camagru-transp-long5.png" alt="Home">
       </a>
-      <div class="navbar-item has-dropdown has-dropdown-up is-hoverable">
-        <a id="help" class="level-item navbar-link">
-          Help
+    </div>
+    <div id="navbarMenu" class="level-right navbar-menu">
+      <div class="level-item navbar-start">
+        <a onclick="loadSnippet('ContactUs')" class="level-item navbar-item">
+          Contact Us
         </a>
+        <div class="navbar-item has-dropdown has-dropdown-up is-hoverable">
+          <a id="help" class="level-item navbar-link">
+            Help
+          </a>
 
-        <div class="navbar-dropdown">
-          <a onclick="loadSnippet('PrivacyPolicy')" class="navbar-item">
-            Privacy Policy
-          </a>
-          <a onclick="loadSnippet('TermsOfUse')" class="navbar-item">
-            Terms of Use
-          </a>
-          <hr class="navbar-divider">
-          <a onclick="loadSnippet('ReportAnIssue')" class="navbar-item">
-            Report an Issue
-          </a>
+          <div class="navbar-dropdown">
+            <a onclick="loadSnippet('PrivacyPolicy')" class="navbar-item">
+              Privacy Policy
+            </a>
+            <a onclick="loadSnippet('TermsOfUse')" class="navbar-item">
+              Terms of Use
+            </a>
+            <hr class="navbar-divider">
+            <a onclick="loadSnippet('ReportAnIssue')" class="navbar-item">
+              Report an Issue
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a href="https://github.com/gwasserfall/camagru" class="button is-light" style="margin-left: -.25rem; font-size: 14px;">
+            © 2019 - gwasserfall & rachel-e-bolton          
+            </a>
+            <a class="button is-primary" href="/users/logout">
+              <strong>Log out</strong>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a href="https://github.com/gwasserfall/camagru" class="button is-light" style="margin-left: -.25rem; font-size: 14px;">
-          © 2019 - gwasserfall & rachel-e-bolton          
-          </a>
-          <a class="button is-primary" href="/users/logout">
-            <strong>Log out</strong>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
+  </nav>
+</div>
